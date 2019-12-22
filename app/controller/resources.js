c28dbbc730d523d75db35b1153144670ae265bb5
孙晓宇 <sunxiaoyu@sunxiaoyudeMac-Pro.local>
2019-12-22 18:17:29 +0800

    update

---
+++ app/controller/resources.js
@@ -0,0 +1,18 @@
+// app/controller/users.js
+const Controller = require('egg').Controller;
+
+function toInt(str) {
+  if (typeof str === 'number') return str;
+  if (!str) return str;
+  return parseInt(str, 10) || 0;
+}
+
+class ResourceController extends Controller {
+  async index() {
+    const ctx = this.ctx;
+    const query = { limit: toInt(ctx.query.limit), offset: toInt(ctx.query.offset) };
+    ctx.body = await ctx.model.Resources.findAll(query);
+  }
+}
+
+module.exports = ResourceController;
--- test/factories.js
+++ test/factories.js
@@ -6,7 +6,7 @@ module.exports = app => {
   app.factory = factory;
 
   // define user and default data
-  factory.define('resources', app.model.Resource, {
+  factory.define('resources', app.model.resource, {
     application_id: 12345,
     resource_name: factory.sequence('Resource.resource_name', n => `rs_${n}`),
     creator: 9284,
